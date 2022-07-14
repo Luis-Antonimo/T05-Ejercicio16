b948fdab1f868d142efa6e83e1b0d8407140b783
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-14 20:40:57 +0200

    1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace T05Ejercicio16
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 1, 5, 4, 3, 3, 2, 1 };
+            //Utilizo el método Reverse de System.Linq
+            //para invertir los valores de un array
+            int[] arrayInverso = array.Reverse().ToArray();
+
+            Console.WriteLine("Los elementos del array original por orden son: ");
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Los elementos del array original invertidos son: ");
+            foreach (int i in arrayInverso)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
